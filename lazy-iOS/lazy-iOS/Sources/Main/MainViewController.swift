e49601f7225be3b67a298f1a8dc1b8a55db13b49
inaeLee <inae9709@hanmail.net>
2021-07-11 05:46:21 +0900

    add: bubble animation 적용 (UIDynamicAnimator)

--- lazy-iOS/lazy-iOS/Sources/Main/MainViewController.swift
+++ lazy-iOS/lazy-iOS/Sources/Main/MainViewController.swift
@@ -12,6 +12,32 @@ class MainViewController: UIViewController {
 
     // MARK: - Properties
 
+    private lazy var animator = UIDynamicAnimator(referenceView: self.view)
+    private lazy var gravityBehavior = UIGravityBehavior(items: [])
+    private lazy var collisionBehavior = UICollisionBehavior(items: []).then {
+        $0.addBoundary(
+            withIdentifier: "bottom-wall" as NSCopying,
+            for: .init(rect: CGRect(origin: CGPoint(x: 0, y: UIScreen.main.bounds.height - 1), size: CGSize(width: UIScreen.main.bounds.width, height: 1)))
+        )
+
+        $0.addBoundary(
+            withIdentifier: "left-wall" as NSCopying,
+            for: .init(rect: CGRect(origin: .zero, size: CGSize(width: 1, height: UIScreen.main.bounds.height)))
+        )
+
+        $0.addBoundary(
+            withIdentifier: "right-wall" as NSCopying,
+            for: .init(rect: CGRect(origin: CGPoint(x: UIScreen.main.bounds.width - 1, y: 0),
+                                    size: CGSize(width: 1, height: UIScreen.main.bounds.height))
+            )
+        )
+    }
+
+    private lazy var dynamicItemBehavior = UIDynamicItemBehavior(items: []).then {
+        $0.friction = 0.5
+        $0.elasticity = 0
+    }
+
     // MARK: - Initializer
 
     // MARK: - LifeCycle
@@ -20,9 +46,33 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        setView()
+    }
+
     // MARK: - Actions
 
     // MARK: - Methods
+
+    func setView() {
+        for _ in 0 ... 30 {
+            let size = CGFloat([50.0, 65.0, 80.0, 100.0].randomElement()!)
+
+            let bubbleView = UIView(frame: CGRect(x: 50, y: -100, width: size, height: size))
+            bubbleView.cornerRounds()
+            bubbleView.backgroundColor = [UIColor.systemBlue, UIColor.systemPink, UIColor.systemOrange].randomElement()
+
+            view.addSubview(bubbleView)
+
+            gravityBehavior.addItem(bubbleView)
+            collisionBehavior.addItem(bubbleView)
+            dynamicItemBehavior.addItem(bubbleView)
+        }
+
+        animator.addBehavior(gravityBehavior)
+        animator.addBehavior(collisionBehavior)
+        animator.addBehavior(dynamicItemBehavior)
+    }
 
     // MARK: - Protocols
 }
