af884ea6e0bf82d5a0a6d5f07b8958c2268ad166
inaeLee <inae9709@hanmail.net>
2021-07-19 23:53:35 +0900

    update: update Podfile

---
+++ lazy-iOS/lazy-iOS/Sources/Common/Constants/DiviceConstants.swift
@@ -0,0 +1,14 @@
+//
+//  DiviceInfo.swift
+//  lazy-iOS
+//
+//  Created by inae Lee on 2021/07/19.
+//
+
+import UIKit
+
+struct DiviceConstants {
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+    static let safeAreaInset = UIApplication.shared.windows.first?.safeAreaInsets
+}
---
+++ lazy-iOS/lazy-iOS/Sources/Common/Constants/UIComponentsConstants.swift
@@ -0,0 +1,8 @@
+//
+//  ComponentsConstants.swift
+//  lazy-iOS
+//
+//  Created by inae Lee on 2021/07/19.
+//
+
+import Foundation
