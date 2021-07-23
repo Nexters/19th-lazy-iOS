5bfa2db6ab63f738a440db0a2fd269e0ef4a0c4a
inaeLee <inae9709@hanmail.net>
2021-07-24 02:34:11 +0900

    add: icon dummy color extension 추가

---
+++ lazy-iOS/lazy-iOS/Sources/AddHabit/Cell/IconCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  IconCollectionViewCell.swift
+//  lazy-iOS
+//
+//  Created by inae Lee on 2021/07/23.
+//
+
+import UIKit
+
+class IconCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ lazy-iOS/lazy-iOS/Sources/Common/Extensions/UIColor+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  UIColor+Extension.swift
+//  lazy-iOS
+//
+//  Created by inae Lee on 2021/07/24.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(_ red: CGFloat, _ green: CGFloat, _ blue: CGFloat, _ alpha: CGFloat) {
+        self.init(red: red / 255.0, green: green / 255.0, blue: blue / 255.0, alpha: alpha)
+    }
+
+    @nonobjc static var icon1: UIColor { .init(0, 99, 196, 1) }
+    @nonobjc static var icon2: UIColor { .init(255, 211, 53, 1) }
+    @nonobjc static var icon3: UIColor { .init(30, 215, 96, 1) }
+    @nonobjc static var icon4: UIColor { .init(255, 98, 138, 1) }
+    @nonobjc static var icon5: UIColor { .init(299, 299, 299, 1) }
+    @nonobjc static var icon6: UIColor { .init(58, 171, 255, 1) }
+    @nonobjc static var icon7: UIColor { .init(27, 70, 134, 1) }
+    @nonobjc static var icon8: UIColor { .init(255, 88, 88, 1) }
+}
