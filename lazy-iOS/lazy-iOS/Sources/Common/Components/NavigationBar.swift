e308490fb11d5d3ac2971789bc44c5b2b036d410
inaeLee <inae9709@hanmail.net>
2021-07-26 00:44:53 +0900

    fix: light mode 지정하기

---
+++ lazy-iOS/lazy-iOS/Sources/Common/Components/NavigationBar.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationBar.swift
+//  lazy-iOS
+//
+//  Created by inae Lee on 2021/07/25.
+//
+
+import UIKit
+
+class NavigationBar: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
