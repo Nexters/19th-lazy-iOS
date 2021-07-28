dfa88e0db74eca4ab07b3efbebe1d3cd677ee981
inaeLee <inae9709@hanmail.net>
2021-07-28 15:22:42 +0900

    fix: iconCVC inset 변경

--- lazy-iOS/lazy-iOS/Sources/AddHabit/AddHabitViewController.swift
+++ lazy-iOS/lazy-iOS/Sources/AddHabit/AddHabitViewController.swift
@@ -218,7 +218,7 @@ class AddHabitViewController: UIViewController {
         
         iconCollectionView.snp.makeConstraints { make in
             make.top.bottom.equalToSuperview().inset(14)
-            make.leading.trailing.equalToSuperview().inset(16)
+            make.leading.trailing.equalToSuperview()
             make.bottom.equalToSuperview().offset(-14)
         }
         
