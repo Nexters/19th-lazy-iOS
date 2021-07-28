6f8c4f757f8bf5fc3372cf0c0006683f713a2863
inaeLee <inae9709@hanmail.net>
2021-07-28 15:28:48 +0900

    feat: iconCVC scrollToItem 적용

--- lazy-iOS/lazy-iOS/Sources/AddHabit/AddHabitViewController+Extension.swift
+++ lazy-iOS/lazy-iOS/Sources/AddHabit/AddHabitViewController+Extension.swift
@@ -63,6 +63,7 @@ extension AddHabitViewController: UICollectionViewDelegate {
             }
         }
 
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
         isFirst = false
     }
 }
