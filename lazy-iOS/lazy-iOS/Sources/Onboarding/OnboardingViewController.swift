7ed4b9bbeaf2c565f54e5b2b067067601541e12a
inaeLee <inae9709@hanmail.net>
2021-08-01 04:53:46 +0900

    feat: onboarding pageControl + gesture 등록

--- lazy-iOS/lazy-iOS/Sources/Onboarding/OnboardingViewController.swift
+++ lazy-iOS/lazy-iOS/Sources/Onboarding/OnboardingViewController.swift
@@ -44,9 +44,16 @@ class OnboardingViewController: UIViewController {
         $0.image = UIImage(named: "onboarding1")
     }
     
-    private let pageControl = UIPageControl()
+    private let pageControl = UIPageControl().then {
+        $0.numberOfPages = 3
+        $0.pageIndicatorTintColor = .white.withAlphaComponent(0.5)
+        $0.currentPageIndicatorTintColor = .white
+        $0.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+    }
 
     // MARK: - Properties
+    
+    private var currPage: Int = 0
     
     // MARK: - Initializer
     
@@ -55,15 +62,49 @@ class OnboardingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setGesture()
         setConstraints()
     }
     
     // MARK: - Actions
     
+    @objc
+    func handleSwipeGesture(_ gesture: UIGestureRecognizer) {
+        guard let swipeGesture = gesture as? UISwipeGestureRecognizer else { return }
+        
+        switch swipeGesture.direction {
+        case UISwipeGestureRecognizer.Direction.left:
+            currPage += 1
+        case UISwipeGestureRecognizer.Direction.right:
+            currPage -= 1
+        default:
+            break
+        }
+
+        guard (0 ..< pageControl.numberOfPages).contains(currPage) else {
+            currPage = currPage < 0 ? 0 : pageControl.numberOfPages - 1
+            return
+        }
+        
+        pageControl.currentPage = currPage
+        imageView.image = UIImage(named: "onboarding\(currPage + 1)")
+    }
+    
     // MARK: - Methods
     
+    func setGesture() {
+        let rightGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeGesture(_:)))
+        rightGesture.direction = .right
+        
+        let leftGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeGesture(_:)))
+        leftGesture.direction = .left
+        
+        view.addGestureRecognizer(rightGesture)
+        view.addGestureRecognizer(leftGesture)
+    }
+    
     func setConstraints() {
-        view.addSubviews([backgroundImageView, skipButton, mainLabel, subLabel, imageView])
+        view.addSubviews([backgroundImageView, skipButton, mainLabel, subLabel, imageView, pageControl])
         
         backgroundImageView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
@@ -88,6 +129,11 @@ class OnboardingViewController: UIViewController {
             make.top.equalTo(subLabel.snp.bottom).offset(108)
             make.width.equalToSuperview().multipliedBy(299.0 / 375.0)
             make.height.equalTo(imageView.snp.width).multipliedBy(234.0 / 299.0)
+            make.centerX.equalToSuperview()
+        }
+        
+        pageControl.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-78)
             make.centerX.equalToSuperview()
         }
     }
